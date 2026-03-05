2d4e9527c28de883e7c0a9c8d3c693cf07d89fab
Алечка Хехе <cybekolenina@gmail.com>
2026-03-05 21:58:11 +0800

    Этап 1: Добавлен комментарий с ФИО автора

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,17 @@
+﻿//Автор: Агеева Альбина Викторовна. 
+//Группа: ИСП(9)-23-1
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
